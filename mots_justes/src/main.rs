2d606e36cdb3d642d4bc10e03c81daad64b3f2da
Ulysse <ulyssewaintal@gmail.com>
2024-11-03 18:37:05 +0100

    mots justes pas fini

--- mots_justes/src/main.rs
+++ mots_justes/src/main.rs
@@ -4,7 +4,12 @@ use std::{
 
 fn main() {
     let (n, a, b) = parse_input();
+    alg2(n, a, b);
+    // alg1ex(n, a, b);
+    // println!("{}", alg1ex(n, a, b));
+}
 
+fn alg1ex(n: i32, a: Vec<String>, b: Vec<String>) -> usize {
     let mut count = HashMap::<String, usize>::new();
 
     for syla in &a {
@@ -19,51 +24,64 @@ fn main() {
         }
     }
 
-    let correcs = find_corrections(&count);
-    // println!("{correcs:?}");
+    // println!("{count:?}");
+    let mut nums = count.values();
 
-    let correction_maj = test(correcs.clone());
-    let mut res = 0;
-    for (key, c) in count.iter() {
-        // println!("{key}");
-        if *c < correction_maj {
-            let mut tested = count.clone();
-            tested.remove(key);
-            if dbg!(test(find_corrections(&tested))) >= correction_maj {
-                res += correcs[key];
-            } else {
-                res += c;
+    let res = nums.map(|c| {
+        find_corrections(&count, *c as i32).values().sum::<usize>()
+    }).min().unwrap();
+
+    println!("{:?}", res);
+
+    res
+
+}
+
+fn alg2(n: i32, a: Vec<String>, b: Vec<String>) {
+    let mut count = HashMap::<String, usize>::new();
+    for sa in &a {
+        for sb in &b {
+            let word = format!("{}{}", sa, sb).to_string();
+            match count.get_mut(&word) {
+                Some(r) => *r += 1,
+                None => { count.insert(word, 1).unwrap(); },
             }
-        } else {
-            res += correcs[key];
         }
-    } 
-    println!("{res}");
+    }
 
+    let mut count_zones = HashMap::<usize, usize>::new();
+    for (_, v) in count.iter() {
+        match count_zones.get_mut(v) {
+            Some(r) => *r += 1,
+            None => { count_zones.insert(*v, 1).unwrap(); },
+        }
+    }
+
+    let mut sum_zones = vec![];
+    for i in 0..count_zones.len() {
+        
+    }
+    
 }
 
 fn test(count: HashMap<String, usize>) -> usize {
     count.values().sum()
 }
 
-fn find_corrections(count: &HashMap<String, usize>) -> HashMap<String, usize> {
-    // println!("{count:?}");
-    let array = &mut (count.values().collect::<Vec<_>>());
-    array.sort();
-    let target = *array[count.len() / 2] as i32;
-
-    // println!("{target}");
-
+fn find_corrections(count: &HashMap<String, usize>, target: i32) -> HashMap<String, usize> {
     let mut corecs = HashMap::<String, usize>::new();
 
     count.iter().for_each(|(k, v)| {
-        let dif = (*v as i32 - target).abs() as usize;
+        let mut dif = (*v as i32 - target).abs() as usize;
+        dif = std::cmp::min(*v, dif);
         if corecs.contains_key(k) {
             *corecs.get_mut(k).unwrap() += dif;
         } else {
             corecs.insert(k.to_owned(), dif);
         }
     });
+
+    println!("target = {} : {}", target, corecs.values().sum::<usize>());
 
     corecs
 }
@@ -89,4 +107,27 @@ fn parse_input() -> (i32, Vec<String>, Vec<String>) {
     }
     let b = input.trim().split('\n').map(|x| x.to_string()).collect::<Vec<_>>();
     (n, a, b)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use rand::Rng;
+
+    #[test]
+    fn test_alg() {
+        const N: usize = 3;
+
+        let letters = (0..N).map(|x| x.to_string()).collect::<Vec<_>>();
+        let l1 = vec![];
+        let l2 = vec![];
+
+        for _ in 0..N {
+            l1.push(letters[rand::thread_rng().gen_range(0..N)]);
+            l2.push(letters[rand::thread_rng().gen_range(0..N)]);
+        }
+
+        assert_eq!(alg1ex(N, l1, l2), alg2(N, l1, l2));
+
+    }
 }
